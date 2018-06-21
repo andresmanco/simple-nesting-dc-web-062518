4d697e5c898dcf1ece4625bffc473a9b7327de1f
Andres Manco <andresmanco@gmail.com>
2018-06-21 21:58:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
    
      alans_new_info = "GUI"
      programmer_hash[:alan_kay].each do |prog|
-     programmer_hash[:dennis_ritchie][:known_for] = alans_new_info
+     prog[:known_for] = alans_new_info
      
      
 end
